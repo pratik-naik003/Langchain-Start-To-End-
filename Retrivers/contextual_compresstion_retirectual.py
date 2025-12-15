b94977a2f647f74c04f9e464477cf0ab7babbdbc
pratik naik <p0241naik@gmail.com>
2025-12-15 19:36:27 +0530

    contextual compresstion retrieval file added

---
+++ Retrivers/contextual_compresstion_retirectual.py
@@ -0,0 +1,66 @@
+import langchain
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+from langchain_core.documents import Document
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.text_compressors import LLMChainExtractor
+
+docs = [
+    Document(page_content=(
+        """The Grand Canyon is one of the most visited natural wonders in the world.
+        Photosynthesis is the process by which green plants convert sunlight into energy.
+        Millions of tourists travel to see it every year. The rocks date back millions of years."""
+    ), metadata={"source": "Doc1"}),
+
+    Document(page_content=(
+        """In medieval Europe, castles were built primarily for defense.
+        The chlorophyll in plant cells captures sunlight during photosynthesis.
+        Knights wore armor made of metal. Siege weapons were often used to breach castle walls."""
+    ), metadata={"source": "Doc2"}),
+
+    Document(page_content=(
+        """Basketball was invented by Dr. James Naismith in the late 19th century.
+        It was originally played with a soccer ball and peach baskets. NBA is now a global league."""
+    ), metadata={"source": "Doc3"}),
+
+    Document(page_content=(
+        """The history of cinema began in the late 1800s. Silent films were the earliest form.
+        Thomas Edison was among the pioneers. Photosynthesis does not occur in animal cells.
+        Modern filmmaking involves complex CGI and sound design."""
+    ), metadata={"source": "Doc4"})
+]
+
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+
+vectorstore = FAISS.from_documents(docs, embedding_model)
+
+base_retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
+
+# Set up the compressor using an LLM
+model=ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+
+compressor = LLMChainExtractor.from_llm(model)
+
+# Create the contextual compression retriever
+compression_retriever = ContextualCompressionRetriever(
+    base_retriever=base_retriever,
+    base_compressor=compressor
+)
+
+# Query the retriever
+query = "What is photosynthesis?"
+compressed_results = compression_retriever.invoke(query)
+
+for i, doc in enumerate(compressed_results):
+    print(f"\n--- Result {i+1} ---")
+    print(doc.page_content)
